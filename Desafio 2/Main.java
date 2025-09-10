6ca60fc16f02cff3ab900c66fc036408388ca455
amanda <a266881@dac.unicamp.br>
2025-09-10 10:48:14 -0300

    fiz o desafio 3 em .md

---
+++ Desafio 2/Main.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static class ContaBancaria {
+        double saldo;
+
+        ContaBancaria(double saldo) {
+            this.saldo = saldo;
+        }
+
+        void sacar(double valor) {
+            saldo = saldo - valor;
+        }
+
+        void depositar(double valor) {
+            saldo = saldo + valor;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int comando;
+        double valor;
+        boolean repetirOperacao = true;
+        boolean realizarOperacoes = true;
+
+        ContaBancaria conta1 = new ContaBancaria(500);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saldo inicial: 500.0 reais");
+
+        // Executa o código até que o usuário pare de querer realizar modificações
+        while (realizarOperacoes) {
+            // Solicita ao usuário um comando de depósito ou saque, até que o comando seja válido
+            while (repetirOperacao) {
+                System.out.print("Para sacar, digite '1'\nPara depositar, digite '2'\nComando: ");
+                comando = scanner.nextInt();
+                System.out.print("Digite o valor do saque/depósito, em reais: ");
+                valor = scanner.nextDouble();
+                if (comando == 1) {
+                    if (valor > conta1.saldo) {
+                        System.out.println("Saldo insuficiente para saque!");
+                    } else {
+                        conta1.sacar(valor);
+                    }
+                    repetirOperacao = false;
+                }  else if (comando == 2) {
+                    conta1.depositar(valor);
+                    repetirOperacao = false;
+                } else {
+                    System.out.println("Comando inválido, tente novamente");
+                }
+            }
+
+            System.out.println();
+            System.out.println("Saldo em conta: " + conta1.saldo + " reais");
+            System.out.println();
+
+            System.out.println("Deseja realizar outra operação?\nSe sim, digite '1'\nSe não, digite '2'\nComando: ");
+            comando = scanner.nextInt();
+                if (comando == 1) {
+                    System.out.println("Ótimo! Você irá realizar outra operação!");
+                    repetirOperacao = true;
+                }
+                else if (comando == 2) {
+                    realizarOperacoes = false;
+                } else {
+                    System.out.println("Comando inválido, outra operação será realizada.");
+                    repetirOperacao = true;
+                }
+        }
+        System.out.println();
+        System.out.println("Saldo final: " + conta1.saldo);
+
+        scanner.close();
+    }
+}
---
+++ Desafio 3/desafio3.md
@@ -0,0 +1,23 @@
+## Como pedir ajuda para uma IA (LLM) com dado sensível no código
+
+Considerando que tratam-se de informações sensíveis, deve-se tomar cuidado durante a solicitação
+para que os dados confidenciais não sejam expostos. Logo, é possível solicitar auxílio com o código
+alterando os valores dos atributos e variáveis para valores genéricos, como feito no exemplo:
+
+==============================================================
+Estou desenvolvendo em Java e tenho a seguinte classe inicial:
+public class ApiService {
+    private static final String API_TOKEN = "YOUR_TOKEN_HERE";
+    
+        public void conectar() {
+            // TODO: lógica para consumir API
+        }
+    }
+
+Sugira-me boas práticas de programação (como organização da classe,
+segurança do token, tratamento de erros, etc.) e me forneça um exemplo
+de código em Java para consumir uma API externa dentro do método conectar,
+incluindo como enviar o token corretamente no cabeçalho da requisição.
+===============================================================
+
+Deste modo, os dados com que está-se trabalhando ficarão protegidos e será possível solicatar ajuda para a IA
