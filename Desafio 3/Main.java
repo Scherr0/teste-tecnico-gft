e2311dfe689a3c1f2acd294e8366e8d2c7e5f539
amanda <a266881@dac.unicamp.br>
2025-09-10 10:36:44 -0300

    desafios 1 e 2

---
+++ Desafio 3/Main.java
@@ -0,0 +1,30 @@
+
+
+public class Main {
+
+    static class Contato {
+        String nome;
+        String telefone;
+
+        Contato(String nome, String telefone) {
+            this.nome = nome;
+            this.telefone = telefone;
+        }
+
+        String exibir() {
+            return "Nome: " + nome + ", Telefone: " + telefone;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Contato c1 = new Contato("Maria", "(11) 94509-9452");
+        Contato c2 = new Contato("João", "(11) 99999-0000");
+
+        System.out.println(c1.exibir());
+        System.out.println(c2.exibir());
+
+
+    }
+}
